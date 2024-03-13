94dc70a1aafcdfce4a5e6a51173b755f9921f825
YEPES-HIGUITA <juanferhiguita65@gmail.com>
2024-03-12 21:28:33 -0500

    Reestructura carpetas

---
+++ src/Figuras/Rectangulo.java
@@ -0,0 +1,21 @@
+
+//Extendemos la clase de figura sobre la que usara sus funciones
+class Rectangulo extends FIgura
+{
+    //Creamos todo como lo hariamos normalmente sin darle el alcance a la clase, simplemente class
+    private double altura;
+    private double base;
+
+    public Rectangulo(double altura, double base)
+    {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    //Usar las funciones abstractas
+    @Override
+    double calcularArea()
+    {
+        return altura * base;
+    }
+}
