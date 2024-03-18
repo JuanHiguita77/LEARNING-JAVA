129d24d542e6c07e9d733b7a4a3ef2e69f4cc827
Juan Fernando <59887137+JuanHiguita77@users.noreply.github.com>
2024-03-18 13:19:34 -0500

    Nuevo tema java con sql jdbc

---
+++ src/java_sql/Main.java
@@ -0,0 +1,9 @@
+import database.ConfigDB;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //Llamar el metodo de conexion
+        ConfigDB.conexionAbierta();
+    }
+}
